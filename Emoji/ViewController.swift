7bbd29046f57845d8ca585fa7eff00c208c13acd
Lisa Chan <Lisachan3.14@gmail.com>
2021-05-09 19:19:42 -0400

    Complete Iteration 3.

--- Emoji/ViewController.swift
+++ Emoji/ViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     let emojis = ["❤️": "Love", "💔": "Heartbreak"]
     @IBAction func showMessage(sender: UIButton) {
-        let selectedEmotion = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "Emotion", message: selectedEmotion, preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        let customMessages = ["Love":["Wonderful!", "Great!", "Congrats!"],"Heartbreak":["Feel Better!", "That's Unfortunate!","Oh no!"]]
+        if let selectedEmotion = sender.titleLabel?.text {
+
+        let titleText = selectedEmotion
+
+            let selectedMessage = customMessages[emojis[selectedEmotion]!]!.randomElement()
+
+        let messageControl = UIAlertController(title: "\(titleText)", message: "\(selectedMessage!)", preferredStyle: UIAlertController.Style.alert)
+
+        messageControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+        present(messageControl, animated: true, completion: nil)
+
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+ 
 
 }
 
+}
