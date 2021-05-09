a38fad941a780e1f0235566b1c9da21b62fd772f
Lisa Chan <Lisachan3.14@gmail.com>
2021-05-09 18:30:59 -0400

    Complete Iteration 1.

--- Emoji/ViewController.swift
+++ Emoji/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Emotion", message: "Response", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
